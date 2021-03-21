1a8c367dac7a63622dd95d90aa0481dcbfd997c5
Nadina Streanga <nadistreanga@gmail.com>
2021-03-21 10:47:50 +0200

    Initial Commit

---
+++ src/main/java/app/demo/carryonnotes/CarryonnotesApplication.java
@@ -0,0 +1,13 @@
+package app.demo.carryonnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarryonnotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarryonnotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/demo/carryonnotes/CarryonnotesApplicationTests.java
@@ -0,0 +1,13 @@
+package app.demo.carryonnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarryonnotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
